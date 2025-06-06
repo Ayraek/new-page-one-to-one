1775e2b3f8bf7e5201395099c90a361814aec1ad
Антон Пяточенко <antonpyatochenko@MacBook-Air-Anton.local>
2025-06-06 16:14:15 +0300

    Локальные правки в Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Hero.module.css';
-import FloatingObjects from './FloatingObjects';
 
 const Hero: React.FC = () => {
   return (
     <section className={styles.hero}>
-      <FloatingObjects />
       <div className={styles.content}>
-        <h1>Профессия "Продакт-менеджер" с нуля <br />под руководством наставника</h1>
+        <h1>
+          Профессия Продукт-менеджер с нуля <br />
+          под руководством наставника
+        </h1>
         <p>Индивидуальное обучение с практикующими PM из топовых IT-компаний РФ</p>
         <button>Записаться на бесплатную консультацию</button>
       </div>
