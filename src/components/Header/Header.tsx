2e1f06987e96c0582fe68a014df4ac19e728dfd7
Антон Пяточенко <antonpyatochenko@MacBook-Air-Anton.local>
2025-06-06 16:26:56 +0300

    Завершение ребейза: правки Hero.module.css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>One2One</div>
+      <div className={styles.logo}>One to One</div>
       <button className={styles.button}>Записаться</button>
     </header>
   );
